945261c71ea008c36c75cedabe70605e65351b1d
Rodrigo Barbosa <uspinhal@yahoo.com>
2020-10-16 20:47:59 -0300

    Exercício da Semana 1

---
+++ Data.java
@@ -0,0 +1,37 @@
+public class Data {
+
+    private int dia;
+    private int mes;
+    private int ano;
+    public Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+// Métodos getters
+    public int getDia(){
+        return dia;
+    }
+    public int getMes(){
+        return mes;
+    }
+    public int getAno(){
+        return ano;
+    }
+// Método Setter
+    public void setData(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+// Método toString
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;      
+    }
+// Método trocarCom
+    public void trocarCom(Data d2) {
+        Data auxData = new Data(this.dia, this.mes, this.ano);
+        this.setData(d2.dia, d2.mes, d2.ano);
+        d2.setData(auxData.dia, auxData.mes, auxData.ano);
+    }
+}
---
+++ DataTeste.java
@@ -0,0 +1,22 @@
+public class DataTeste {
+
+    public static void main(String[] args) {
+        Data d1 = new Data(10, 10, 2013);
+        System.out.println("Dia: " + d1.getDia());
+        System.out.println("Mês: " + d1.getMes());
+        System.out.println("Ano: " + d1.getAno());
+        System.out.println(d1.toString());
+        d1.setData(14, 01, 2014);
+        System.out.println("Dia: " + d1.getDia());
+        System.out.println("Mês: " + d1.getMes());
+        System.out.println("Ano: " + d1.getAno());
+        System.out.println("d1: " + d1.toString());
+
+        Data d2 = new Data(27, 04, 1981);
+        System.out.println("d2: " + d2.toString());
+        
+        d1.trocarCom(d2);
+        System.out.println("d1: " + d1.toString());
+        System.out.println("d2: " + d2.toString());
+    }
+}
