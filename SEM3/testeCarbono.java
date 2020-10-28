af228e5b915e35d2b4a4e501f7bcfda40e4fea66
Rodrigo Barbosa <uspinhal@yahoo.com>
2020-10-27 23:34:08 -0300

    SEM3 - Criado main

---
+++ SEM3/testeCarbono.java
@@ -0,0 +1,12 @@
+package SEM3;
+
+import SEM3.Carbono.Casa;
+
+public class testeCarbono {
+    public static void main(String[] args) {
+        Casa casa = new Casa(1,2,3,4,5);
+
+        System.out.println(casa.getNumeroPessoas());
+        System.out.println(casa.getCarbonoEmitido());
+    }
+}
