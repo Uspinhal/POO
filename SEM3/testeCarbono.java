cf4d5084de0d3e2658e8bfd21d3872b449e310b7
Rodrigo Barbosa <uspinhal@yahoo.com>
2020-10-28 21:31:07 -0300

    SEM3 - Teste da classe Carro

--- SEM3/testeCarbono.java
+++ SEM3/testeCarbono.java
@@ -1,13 +1,13 @@
 package SEM3;
 
-import SEM3.Carbono.Casa;
-import SEM3.Carbono.EmissorCarbono;
+import SEM3.Carbono.*;
 
 public class TesteCarbono {
     public static void main(String[] args) {
         EmissorCarbono casa = new Casa(1,2,3,4,5);
+        EmissorCarbono carro = new Carro("Gasolina", "4.1L", 13000);
 
-        System.out.println();
         System.out.println(casa);
+        System.out.println(carro);
     }
 }
